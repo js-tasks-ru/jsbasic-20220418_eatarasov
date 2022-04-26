321a75dd789a0a14e5aa323add607a9e0220c59a
Evgeny Tarasov <eatarasov@yahoo.com>
2022-04-26 15:04:59 +0300

    module-2, task-1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let i of Object.values(salaries)) {
+    if (typeof i === "number" && isFinite(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
