d54c6c7ea74cf5544f0608d4a8dbd34aaf72265a
Evgeny Tarasov <eatarasov@yahoo.com>
2022-05-09 11:49:37 +0300

    module-4, task-2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,12 @@
+
 function makeDiagonalRed(table) {
-  // ваш код...
-}
+    for (let i = 0; i < table.rows.length; i++) {
+      for (let j = i; j < table.rows[i].cells.length; j++) {
+        if (i === j) {
+          table.rows[i].cells[j].style.backgroundColor = 'red';
+          break;
+        }
+    }
+}};
+
+
