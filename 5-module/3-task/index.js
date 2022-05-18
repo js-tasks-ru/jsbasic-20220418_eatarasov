55e44a1eed57f99ea5655d872af77f90f4578c15
Evgeny Tarasov <eatarasov@yahoo.com>
2022-05-18 14:09:56 +0300

    module-5, task-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  let rightButton = document.querySelector(".carousel__arrow_right");
+  let leftButton = document.querySelector(".carousel__arrow_left");
+  let slidesFrame = document.querySelector(".carousel__inner");
+  let slidesArrayLength = document.querySelectorAll(".carousel__slide").length;
+
+  let counter = 1;
+  let slideWidth = 988;
+  let position = 0;
+
+  rightButton.addEventListener("click", function () {
+    counter++;
+    position += slideWidth * -1;
+    setPosition();
+    console.log(position);
+    checkButton();
+  });
+
+  leftButton.addEventListener("click", function () {
+    counter--;
+    position += slideWidth;
+    setPosition();
+    console.log(position);
+    checkButton();
+  });
+
+  checkButton();
+
+  function setPosition() {
+    slidesFrame.style.transform = `translateX(${position}px)`;
+  }
+
+  function checkButton() {
+    rightButton.style.display = (counter === slidesArrayLength) ? 'none' : '';
+    leftButton.style.display = (counter === 1) ? 'none' : '';
+  };
 }
